aedc5530783b0ff7313f9889f73b1967831404dd
auroraloreti <loretiaurora16@gmail.com>
2024-06-25 11:18:44 +0200

    esercizio custom hook 3 svolto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import GithubUsers from './components/GithubUsers';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Search GitHub Users</h1>
+      <GithubUsers />
+    </div>
+  );
+};
+
+export default App;
+
+export default App
